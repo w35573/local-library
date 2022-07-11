9b4a3fb11d2799decce16e06f9ffcb8f764d2612
w35573 <79666165+w35573@users.noreply.github.com>
2022-07-11 17:52:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,13 +13,16 @@ var helmet = require('helmet');
 
 var app = express();
 
-app.use(helmet());
 
-//mongoose setup with atlas
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://w35573:Om0RMX4NP9WbEfIm@cluster0.vc4ea63.mongodb.net/?retryWrites=true&w=majority')
-  .then(() => console.log('Connected to MongoDB...'))
-  .catch(err => console.error('Could not connect to MongoDB...', err));
+// Set up mongoose connection
+var mongoose = require('mongoose');
+var dev_db_url = 'mongodb+srv://w35573:Om0RMX4NP9WbEfIm@cluster0.vc4ea63.mongodb.net/?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -29,22 +32,22 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-app.use(compression()); //Compress all routes
+app.use(helmet());
+app.use(compression()); // Compress all routes
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/catalog', catalogRouter); 
+app.use('/catalog', catalogRouter);  // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
